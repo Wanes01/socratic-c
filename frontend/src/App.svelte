64958b20bb6bffef783693a2808c711ff44dda4c
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-04-04 18:46:21 +0200

    created files tree api

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,16 @@
 <script>
+	// definisci la promise ma non la spetti qui
+	const response = fetch("api/test").then((res) => res.json());
 </script>
 
 <main>
-  <p class="text-red-600 font-bold">
-    Ciao! Questo è un test per la configurazione con tailwind!
-  </p>
+	<p class="text-red-600 font-bold">
+		Ciao! Questo è un test per la configurazione con tailwind! Questo è il
+		tuo numero:
+		{#await response}
+			IN CARICAMENTO FIGA
+		{:then numero}
+			{numero.numero}
+		{/await}
+	</p>
 </main>
--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express();
 // Serve i file statici prodotti da Svelte
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
+const filesRouter = require('./routes/files')
+app.use('/api/files', filesRouter)
+
+
 // Le tue rotte API
-app.get('/api/esercizio', (req, res) => {
-    res.json({ title: "Hello C" });
+app.get('/api/test', (req, res) => {
+    res.json({ numero: "100000000000" });
 });
 
 // Per ogni altra rotta, rimanda all'index.html di Svelte (fondamentale per le SPA)

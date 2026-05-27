c7c4c309311cdb47d1d538977b29e123e429ac88
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-05-27 18:22:37 +0200

    modified vite comments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 5173, // vite port
     proxy: {
-      // redirects every /api to the backend container
+      // redirects every /api request to the backend service
       '/api': {
         target: 'http://backend:5000',
         changeOrigin: true,
